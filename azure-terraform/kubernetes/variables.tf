766715cdd17bfe9d9296d833ba4d045f221f3e65
Stiven <stiven.castellon@nuevatel.com>
2021-05-01 18:11:39 -0500

    first commit

---
+++ azure-terraform/kubernetes/variables.tf
@@ -0,0 +1,29 @@
+variable client_id {}
+variable client_secret {}
+variable ssh_public_key {}
+
+variable environment {
+    default = "dev"
+}
+
+variable location {
+    default = "westeurope"
+}
+
+variable node_count {
+  default = 2
+}
+
+
+
+variable dns_prefix {
+  default = "k8sAforo255test"
+}
+
+variable cluster_name {
+  default = "k8sAforo255test"
+}
+
+variable resource_group {
+  default = "kubernetesAforo255"
+}
